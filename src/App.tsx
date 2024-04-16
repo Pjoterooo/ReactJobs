5614371fb078926e8f9da2ffd1b3b07d74817b6f
Piotr <piotrsala02@gmail.com>
2024-04-16 21:28:55 +0200

    commitReactApp

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Hero } from "./components/Hero/Hero";
+import { HomeCards } from "./components/HomeCards/HomeCards";
+import { JobListings } from "./components/JobListings/JobListings";
+import Navbar from "./components/Navbar/Navbar";
+import { ViewAllJobs } from "./components/ViewAllJobs/ViewAllJobs";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+}
+
+export default App;
---
+++ src/types/types.tsx
@@ -0,0 +1,18 @@
+export interface JobsInterface {
+  id: string;
+  title: string;
+  type: string;
+  description: string;
+  location: string;
+  salary: string;
+  company: {
+    name: string;
+    description: string;
+    contactEmail: string;
+    contactPhone: string;
+  };
+}
+
+export interface JobListingInterface {
+  job: JobsInterface;
+}
