a9a6b15d335d5df4c9efe588868799c23332597d
Piotr <piotrsala02@gmail.com>
2024-04-17 10:10:49 +0200

    addComponentsToHomePages

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,9 +1,15 @@
 import Hero from "../src/components/Hero/Hero";
+import HomeCards from "../src/components/HomeCards/HomeCards";
+import JobListings from "../src/components/JobListings/JobListings";
+import ViewAllJobs from "../src/components/ViewAllJobs/ViewAllJobs";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <HomeCards/>
+      <JobListings/>
+      <ViewAllJobs/>
     </>
   );
 };
