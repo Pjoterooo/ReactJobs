c1afa474cced5d2a6c4c6e7ff3500a85ca3f6bc8
Piotr <piotrsala02@gmail.com>
2024-04-17 13:47:00 +0200

    addAddJobPageAndTypeWritter

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,23 @@
+import { Typewriter } from "react-simple-typewriter";
+
 const Hero = () => {
+  const word = ["Become a React Dev"];
   return (
     <section className="bg-indigo-700 py-20 mb-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-            Become a React Dev
+            <Typewriter
+              words={word}
+              loop={10}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={75}
+              delaySpeed={1000}
+            />
           </h1>
+
           <p className="my-4 text-xl text-white">
             Find the React job that fits your skills and needs
           </p>
