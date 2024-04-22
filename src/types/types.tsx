93a4d1410ba33b775c16aa4f8b5cc2b6f8e1a361
Piotr <piotrsala02@gmail.com>
2024-04-22 13:33:48 +0200

    addJobFormComponents

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -32,3 +32,25 @@ export interface AddJobPageInterface {
 export interface JobPageInterface {
   deleteJob: (id: string | undefined) => Promise<void>;
 }
+
+export interface AddJobFormInterface{
+  submitForm: (e) => void
+  title: string;
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+  type: string;
+  setType: React.Dispatch<React.SetStateAction<string>>;
+  description: string;
+  setDescription: React.Dispatch<React.SetStateAction<string>>;
+  location: string;
+  setLocation: React.Dispatch<React.SetStateAction<string>>;
+  salary: string;
+  setSalary: React.Dispatch<React.SetStateAction<string>>;
+  companyName: string;
+  setCompanyName: React.Dispatch<React.SetStateAction<string>>;
+  companyDescription: string;
+  setCompanyDescription: React.Dispatch<React.SetStateAction<string>>;
+  companyEmail: string;
+  setCompanyEmail: React.Dispatch<React.SetStateAction<string>>;
+  companyPhone: string;
+  setCompanyPhone: React.Dispatch<React.SetStateAction<string>>;
+}
